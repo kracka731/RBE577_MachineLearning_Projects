fixed gradient calcs, cos&sin features

Assignment_1_Fillable/feature_engineering.py
@@ -19,10 +19,15 @@ def engineer_features(angles):
         (1, 42)
     """
 
-    #Your code here
+    n_samples, n_inputs = np.shape(angles)
+    features = angles.copy()
 
-    # Use sin(q_i),cos(q_i) for each q
+    for q_i in range(1, n_inputs+1):
+        # Find the sum of the current joint angle and all preceeding joint angles
+        tot_ang = np.sum(angles[:, :q_i], axis=1)
+        # Extract effects of angles on xyz based on cos & sin
+        cos = np.reshape(np.cos(tot_ang), (n_samples, 1))
+        sin = np.reshape(np.sin(tot_ang), (n_samples, 1))
+        features = np.hstack((features, cos, sin))
 
-
-
-    pass
+    return features

Assignment_1_Fillable/lin_regression_sgd.py
@@ -59,9 +59,10 @@ class SGDLinearRegression:
         Returns:
             tuple: Weight gradients and bias gradients
         """
+        n_samples, n_outputs = np.shape(y_true)
         error = y_pred - y_true 
-        weight_gradient = -2 * np.mean(X*error)
-        bias_gradient = -2 * np.mean(error)
+        weight_gradient = -2/n_samples * (X.T @ error)
+        bias_gradient = -2 * np.mean(error, axis=0)
         return (weight_gradient, bias_gradient)
 
     def fit(self, X:np.ndarray, y:np.ndarray, batch_size=32, epochs=100):
@@ -88,7 +89,7 @@ class SGDLinearRegression:
                 X_batch = data[i:j, :np.size(X, axis=1)]
                 y_batch = data[i:j,  np.size(X, axis=1):]
                 # print(f"dims of weights slice: {np.shape(self.weights)}, now T: {np.shape(self.weights.T)}, X batch; {np.shape(X_batch)}")
-                y_pred = X_batch @ self.weights# (self.weights @ X_batch.T).T
+                y_pred = X_batch @ self.weights# =(self.weights.T @ X_batch.T).T
                 # print(f"y pred shape: {np.shape(y_pred)}")
 
                 # Compute gradient & weights for this batch