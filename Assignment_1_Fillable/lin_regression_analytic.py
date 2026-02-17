6486750a07c67c39ea413d97ac3a535f5617eb12
kracka731 <kracka@wpi.edu>
2026-02-17 15:46:30 -0500

    intercept, feature engineering

--- Assignment_1_Fillable/lin_regression_analytic.py
+++ Assignment_1_Fillable/lin_regression_analytic.py
@@ -32,17 +32,21 @@ class AnalyticalLinearRegression:
         # X (number of samples, [th1, th2, th3, th4, th5, th6])
         # y (number of samples,  [x,y,z,rx,ry,rz])
 
+        # Add column to represent the intercept/bias
+        n_samples, n_inputs = np.shape(X)
+        X = np.hstack((np.ones((n_samples, 1)), X))
+        
         # We will be using the Ordinary Least Squares method
-
         XT = np.transpose(X)
-        X_cross_inv = np.linalg.inv(XT @ X) # The matrix of feature cross products
+        XTX_inv = np.linalg.inv(XT @ X) # The matrix of feature cross products
 
         # Find the estimated coefficient vector and label as weights
-        self.weights = X_cross_inv@XT@y
+        self.weights = XTX_inv@XT@y
 
         # If the determinant of a matrix is 0, it is singular
-        if np.linalg.det(self.weights) == 0:
-            return False
+        n_inputs, n_outputs = np.shape(self.weights)
+        if n_inputs == n_outputs: 
+            return np.linalg.det(self.weights) != 0
         else:
             return True
 
@@ -58,15 +62,18 @@ class AnalyticalLinearRegression:
 
         # print("weights: ",self.weights,"\n")
         # print("X: ",X,"\n")
+        # Add column to represent the intercept/bias
+        n_samples, n_inputs = np.shape(X)
+        X = np.hstack((np.ones((n_samples, 1)), X))
 
-        y_pred = self.weights@X.T
+        y_pred = X @ self.weights
 
-        return y_pred.T
+        return y_pred
 
 if __name__ == "__main__":
 
     #############Your CODE STARTS HERE##############
-    use_engineered_features = False 
+    use_engineered_features = True 
 
     # Load data
     X_train, X_test, y_train, y_test = prepare_dataset("data/ur10_dataset.csv")
