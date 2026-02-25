ce3af68054e6b50204b5dd3c4d3e439ad3b1215d
jkuehne42 <jasonakuehne@gmail.com>
2026-02-25 17:34:16 -0500

    nn setup

---
+++ Project_2/src/train.py
@@ -0,0 +1,15 @@
+# Expected in the Project2.pdf
+
+# Neural Network Model
+# Multi-layer perceptron
+
+# TODO
+# Define an MLP with at least one hidden layer. 
+# Train the network using the given dataset.
+# Compute and visualize loss curves.
+# Compare the network's performance with the physics model.
+
+# models.py used in lib folder
+# custom.yaml in config
+# Input data are "push parameters"(unsure where to definitively locate for moves. Also in data_x?) & data_x_... from data/genesis folder
+# Output data is the resulting state (likely data_y_...) (need to parse these from .npy)
