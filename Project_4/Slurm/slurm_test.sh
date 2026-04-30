53fd9e7651dfe0656bd7678261b5b339b812eb76
Katarzyna <kracka@wpi.edu>
2026-04-30 18:59:39 -0400

    hello world

--- Project_4/Slurm/slurm_test.sh
+++ Project_4/Slurm/slurm_test.sh
@@ -3,15 +3,15 @@
 #SBATCH --mem=4G
 #SBATCH --output=output.txt           # Standard output file
 #SBATCH --error=error.txt             # Standard error file
-#SBATCH --partition=partition_name    # Partition or queue name
+##SBATCH --partition=partition_name    # Partition or queue name
 #SBATCH --nodes=1                     # Number of nodes
 #SBATCH --ntasks-per-node=1           # Number of tasks per node
 #SBATCH --cpus-per-task=1             # Number of CPU cores per task
 #SBATCH --time=0:02:00                # Maximum runtime (D-HH:MM:SS)
 #SBATCH --mail-type=END               # Send email at job completion
-#SBATCH --mail-user=JKuehne@wpi.edu    # Email address for notifications
+#SBATCH --mail-user=kracka@wpi.edu    # Email address for notifications
 #SBATCH -A rbe577 # for RBE577 P3
 #SBATCH -p academic # for RBE577 P3
 #SBATCH --mem=4G
 module load python
-python hello_script.py
+python Slurm/hello_script.py
