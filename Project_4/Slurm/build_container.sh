a36eb0d0fecec8c7c6faaed7cef0d276957a1052
Katarzyna (Kasia) Racka <71733344+kracka731@users.noreply.github.com>
2026-05-01 19:30:23 -0400

    Bc train functional to turing  (#6)
    * Functional turing cluster setup to operate the bc_rnn training. For bc_test however, run sinteractive and copy code in.
    
    * bc_test.sh fix for use with sbatch
    
    ---------
    
    Co-authored-by: jkuehne42 <jasonakuehne@gmail.com>

--- Project_4/Slurm/build_container.sh
+++ Project_4/Slurm/build_container.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 #SBATCH -A rbe577 ## for RBE577 P4
 #SBATCH -p academic ## for RBE577 P4
-#SBATCH --job-name "container_setup"
+#SBATCH --job-name=container_setup
 #SBATCH --mem=50G
 #SBATCH --output=container_setup_out.txt           # Standard output file
 #SBATCH --error=container_setup_error.txt             # Standard error file
 #SBATCH --nodes=1                     # Number of nodes
 #SBATCH --ntasks-per-node=1           # Number of tasks per node
-#SBATCH --cpus-per-task 25             # Number of CPU cores per task
-#SBATCH --gres=gpu:1
-#SBATCH --time 0-01:00:00                # Maximum runtime (D-HH:MM:SS)
+#SBATCH --cpus-per-task=25             # Number of CPU cores per task
+#SBATCH --time=01:00:00                # Maximum runtime (D-HH:MM:SS)
 #SBATCH --mail-type=END               # Send email at job completion
 #SBATCH --mail-user=kracka@wpi.edu 
 
 module load apptainer 
 
-apptainer build --force box.sif Slurm/apptainer_def.def
+apptainer build --force box.sif ./apptainer_def.def
