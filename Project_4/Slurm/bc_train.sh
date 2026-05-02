2e92065ec78d785d53630656cd0218d8b2fd45a2
jkuehne42 <jasonakuehne@gmail.com>
2026-05-02 02:33:20 -0400

    Functional training through Behavioral Cloning

--- Project_4/Slurm/bc_train.sh
+++ Project_4/Slurm/bc_train.sh
@@ -20,7 +20,7 @@ PROJECT_DIR=${SLURM_SUBMIT_DIR}
 echo "Working from: ${PROJECT_DIR}"
 
 # apptainer exec --userns ${PROJECT_DIR}/Slurm/box.sif pip install robomimic
-# apptainer exec --userns ${PROJECT_DIR}/Slurm/box.sif pip install robosuite==1.4.1 --force-reinstall
+apptainer exec --userns ${PROJECT_DIR}/Slurm/box.sif pip install robosuite==1.5.1 --force-reinstall # may not be required. 
 # apptainer exec --userns ${PROJECT_DIR}/Slurm/box.sif \
 #     python3 -c "import torch; print(torch.__version__)"
 # apptainer exec --userns ${PROJECT_DIR}/Slurm/box.sif \
@@ -30,32 +30,23 @@ echo "Working from: ${PROJECT_DIR}"
 #     python3 -c "import robosuite; print(f'robosuite OK {robosuite.__version__}')"
 
 
-apptainer exec --userns ${PROJECT_DIR}/Slurm/box.sif bash -c \ 'cp -r /usr/local/lib/python3.8/dist-packages/mujoco_py /tmp/mujoco_py_writable'
+# Clean any leftover from a previous run
+rm -rf ${PROJECT_DIR}/mujoco_py_writable
 
+# Copy mujoco_py into the project space
 apptainer exec --userns \
   --bind ${PROJECT_DIR}:/work \
   ${PROJECT_DIR}/Slurm/box.sif \
-  bash -c "cp /work/demonstrations/merged_converted.hdf5 /root/workspace/Project_4/demonstrations/ && \
-           cp /work/training_configs/bc_clean.json /root/workspace/Project_4/training_configs/"
+  bash -c "cp -r /usr/local/lib/python3.8/dist-packages/mujoco_py /work/mujoco_py_writable"
 
-
-
+# Train the model inside the box, using config and demonstration data from outside the box
+echo "Starting training..."
 apptainer exec --userns --nv \
-  --bind /tmp \
-  --bind /tmp/mujoco_py_writable:/usr/local/lib/python3.8/dist-packages/mujoco_py \
   --bind ${PROJECT_DIR}:/work \
-  --bind ${PROJECT_DIR}/bc_trained_models:/root/workspace/Project_4/bc_trained_models \
+  --bind ${PROJECT_DIR}/mujoco_py_writable:/usr/local/lib/python3.8/dist-packages/mujoco_py \
+  --env PYTHONPATH=/root/workspace/Project_4/submodules/robomimic:$PYTHONPATH \
   ${PROJECT_DIR}/Slurm/box.sif \
-  bash -c "yes | python3 -m /root/workspace/Project_4/submodules/robomimic/robomimic/scripts/train.py \
-  --config /root/workspace/training_configs/bc_clean.json \
-  --dataset /root/workspace/demonstrations/merged_converted.hdf5 \
+  bash -c "yes | python3 /root/workspace/Project_4/submodules/robomimic/robomimic/scripts/train.py \
+  --config /work/training_configs/bc.json \
+  --dataset /work/demonstrations/merged_converted.hdf5 \
   --name 'BC_Cloning_Experiment'"
-
-# Copy results back to your directory
-apptainer exec --userns \
-  --bind ${PROJECT_DIR}:/work \
-  ${PROJECT_DIR}/Slurm/box.sif \
-  bash -c "cp -r /root/workspace/Project_4/bc_trained_models/* /work/bc_trained_models/"
-
-echo "Training complete! Results in bc_trained_models/"
-
